0b887c443b32a583d277997af3105d9420203d84
D-Theriot <dylantheriot@gmail.com>
2021-10-02 16:53:24 -0500

    init video rekognition

---
+++ video/rekognition.py
@@ -0,0 +1,23 @@
+import csv
+import boto3
+
+with open('credentials.csv', 'r') as input:
+  next(input)
+  reader = csv.reader(input)
+  for line in reader:
+    access_key_id = line[2]
+    secret_access_key = line[3]
+
+photo = 'video/images/test.jpeg'
+
+client = boto3.client('rekognition', aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key, region_name='us-west-2')
+
+with open(photo, 'rb') as source_image:
+  source_bytes = source_image.read()
+
+  response = client.detect_text(Image={'Bytes': source_bytes })
+
+
+  for words in response['TextDetections']:
+    print(words['DetectedText'])
+
